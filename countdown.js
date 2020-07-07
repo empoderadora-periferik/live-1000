f6ed419fc3a217d804bd5ae08e8c8b183d6fd02b
santiago <ssantiagotl@gmail.com>
2020-07-06 23:17:26 -0300

    Finished working into countdown logic

---
+++ countdown.js
@@ -0,0 +1,34 @@
+//Declaring the deadline button so we can find the difference between current date and final date.
+const deadline = '2020-07-09 20:00:00 gmt-0300';
+
+//Function to parse date and find seconds, minutes, hours etc...
+function getTimeRemaining(endtime) {
+    const total = Date.parse(endtime) - Date.parse(new Date());
+    const seconds = Math.floor((total / 1000) % 60);
+    const minutes = Math.floor((total / 1000 / 60) % 60);
+    const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+    const days = Math.floor(total / (1000 * 60 * 60 * 24));
+
+    return {
+        total,
+        days,
+        hours,
+        minutes,
+        seconds,
+    };
+}
+
+//Clock display output function
+function initializeClock(id, endtime) {
+    const clock = document.getElementById(id);
+    const timeInterval = setInterval(() => {
+        const t = getTimeRemaining(endtime);
+        clock.innerHTML = `${t.days} Dias ${t.hours} horas ${t.minutes} minutos ${t.seconds} segundos`;
+
+        if (t.total <= 0) {
+            clearInterval(timeInterval);
+        }
+    }, 1000);
+}
+
+initializeClock('clock', deadline);
