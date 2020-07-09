1df99f08a000e4cf2fd24a0ae6862638e08d0603
santiago <ssantiagotl@gmail.com>
2020-07-09 18:53:14 -0300

    Fixed popup display time

--- listeners.js
+++ listeners.js
@@ -1,9 +1,12 @@
 // --------------------Popup function--------------------
 
 function onMouseOut(event) {
+
+    if (timeSpentOnSite >= 5) {
     document.removeEventListener("mouseout", onMouseOut);
 
     document.getElementById("popup").style.display = "block";
+}
 }
 
 
@@ -22,3 +25,28 @@ const youtube = document.getElementById("youtube").onclick = function () {
 
 
 document.addEventListener("mouseout", onMouseOut);
+
+// ------------------------User time on page----------------
+
+var timer;
+var timerStart;
+var timeSpentOnSite = getTimeSpentOnSite();
+
+function getTimeSpentOnSite(){
+    timeSpentOnSite = parseInt(localStorage.getItem('timeSpentOnSite'));
+    timeSpentOnSite = isNaN(timeSpentOnSite) ? 0 : timeSpentOnSite;
+    return timeSpentOnSite;
+}
+
+function startCounting(){
+    timerStart = Date.now();
+    timer = setInterval(function(){
+        timeSpentOnSite = getTimeSpentOnSite()+(Date.now()-timerStart);
+        localStorage.setItem('timeSpentOnSite',timeSpentOnSite);
+        timerStart = parseInt(Date.now());
+        // Convert to seconds
+        console.log(parseInt(timeSpentOnSite/1000));
+    },1000);
+}
+
+startCounting();
