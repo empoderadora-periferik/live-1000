24cc73c71e27e99cc1038eec23e47e81dae03b46
santiago <ssantiagotl@gmail.com>
2020-07-10 00:53:59 -0300

    Removed useless script function

--- listeners.js
+++ listeners.js
@@ -1,37 +1,11 @@
-// ------------------------User time on page----------------
 
-var timer;
-var timerStart;
-var timeSpentOnSite = getTimeSpentOnSite();
-
-function getTimeSpentOnSite(){
-    timeSpentOnSite = parseInt(localStorage.getItem('timeSpentOnSite'));
-    timeSpentOnSite = isNaN(timeSpentOnSite) ? 0 : timeSpentOnSite;
-    return timeSpentOnSite;
-}
-
-function startCounting(){
-    timerStart = Date.now();
-    timer = setInterval(function(){
-        timeSpentOnSite = getTimeSpentOnSite()+(Date.now()-timerStart);
-        localStorage.setItem('timeSpentOnSite',timeSpentOnSite);
-        timerStart = parseInt(Date.now());
-        // Convert to seconds
-        console.log(parseInt(timeSpentOnSite/1000));
-    },1000);
-}
-
-startCounting();
 
 // --------------------Popup function--------------------
 
 function onMouseOut(event) {
 
-    if (timeSpentOnSite >= 5) {
-    document.removeEventListener("mouseout", onMouseOut);
-
     document.getElementById("popup").style.display = "block";
-}
+
 }
 
 
