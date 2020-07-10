2d3da05cde724acbafaec56185976a533b22db62
Thyago Lins <33438841+tlsantiago@users.noreply.github.com>
2020-07-10 00:10:53 -0300

    Project BU

---
+++ listeners.js
@@ -0,0 +1,58 @@
+// ------------------------User time on page----------------
+
+var timer;
+var timerStart;
+var timeSpentOnSite = getTimeSpentOnSite();
+
+function getTimeSpentOnSite(){
+    timeSpentOnSite = parseInt(localStorage.getItem('timeSpentOnSite'));
+    timeSpentOnSite = isNaN(timeSpentOnSite) ? 0 : timeSpentOnSite;
+    return timeSpentOnSite;
+}
+
+function startCounting(){
+    timerStart = Date.now();
+    timer = setInterval(function(){
+        timeSpentOnSite = getTimeSpentOnSite()+(Date.now()-timerStart);
+        localStorage.setItem('timeSpentOnSite',timeSpentOnSite);
+        timerStart = parseInt(Date.now());
+        // Convert to seconds
+        console.log(parseInt(timeSpentOnSite/1000));
+    },1000);
+}
+
+startCounting();
+
+// --------------------Popup function--------------------
+
+function onMouseOut(event) {
+
+    if (timeSpentOnSite >= 5) {
+    document.removeEventListener("mouseout", onMouseOut);
+
+    document.getElementById("popup").style.display = "block";
+}
+}
+
+
+// --------------------Consts for ev.listeners--------------------
+
+const closeBtn = document.getElementById("closeBtn");onclick = function () {
+    document.getElementById("popup").style.display = "none"
+}
+
+
+// --------------------Calling listeners--------------------
+
+
+$(function(){
+    var mouseY = 0;
+    var topValue = 0;
+    window.addEventListener("mouseout",function(e){
+        mouseY = e.clientY;
+        if(mouseY<topValue) {
+            onMouseOut();
+        }
+    },
+    false);
+});
